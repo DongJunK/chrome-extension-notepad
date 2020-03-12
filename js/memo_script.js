c0563521a0f7b140ef66b99c00e32e409c66fe24
Junit <skdltm357@nave.com>
2020-03-12 16:33:01 +0900

    modify popup funtion

--- js/memo_script.js
+++ js/memo_script.js
@@ -11,15 +11,14 @@ document.addEventListener('mouseup', function (mousePos) {
 
 chrome.runtime.onConnect.addListener((port)=> {
     port.onMessage.addListener((request)=>{
-        console.log("aasdfasdfads");
         if(request.connect === 'context_menus'){
             document.body.innerHTML += "<div id=\"divpop\" style=\"position:absolute;left:"+request.gPos.pageX+"px;top:"+request.gPos.pageY+"px;z-index:200\"> <table width=300 height=400 cellpadding=2 cellspacing=0> <tr> <td style=\"border:1px #666666 solid\" height=360 align=center bgcolor=white> <p> Memo </p> <textarea id=\"memotext\" name=\"ourtext\" rows=\"17\" style=\"width:95%;height:85%;resize:none\"></textarea> </td> </tr> </table> </div>";
-            document.getElementById("memotext").style.color="#000000";
-            document.getElementById("memotext").style.fontSize = 10;
+            document.getElementById("memotext").style.color = "#000000";
+            document.getElementById("memotext").style.fontSize = "small";
             port.postMessage({result:"success"});
         }else if(request.connect === 'popup'){
-            document.getElementById("memotext").style.color = request.color;
-            document.getElementById("memotext").style.font = request.font;
+            document.getElementById("memotext").style.color = request.fontColor;
+            document.getElementById("memotext").style.fontFamily = request.fontFamily;
             document.getElementById("memotext").style.fontSize = request.fontSize;
             port.postMessage({result:"success"});
         }
--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,55 @@
 'use strict';
 
 
-console.log("start");
+function colorToRgb(fontColor){
+    switch(fontColor){
+        case 'red':
+            fontColor = "#FF0000";
+            break;
+        case 'blue':
+            fontColor = "#00498c";
+            break;
+        case 'black':
+            fontColor = "#000000";
+            break;
+        case 'green':
+            fontColor = "#008000";
+            break;
+        case 'FAB201':
+            fontColor = "#FAD201";
+            break;
+    }
+    return fontColor;
+}
 
 // connect memo_script.js
 function connect(){
-    console.log("aaaaa");
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         var port = chrome.tabs.connect(tabs[0].id);
-        console.log("connect");
-        var font_size = document.getElementById("select_font_size");
-        console.log(font_size);
-        var font_family = document.getElementById("select_font_family");
-        var font_color = document.getElementById("select_font_color");
-        port.postMessage({ connect:"popup", fontSize:font_size, font:font_family, color:font_color});
+        var select_font_size = document.getElementById("select_font_size");
+        var select_font_family = document.getElementById("select_font_family");
+        var select_font_color = document.getElementById("select_font_color");
+        var fontSize = select_font_size.options[select_font_size.selectedIndex].value;
+        var fontFamily = select_font_family.options[select_font_family.selectedIndex].value;
+        var fontColor = select_font_color.options[select_font_color.selectedIndex].value;
+        
+        fontColor = colorToRgb(fontColor);
+        
+        port.postMessage({ connect:"popup", fontSize:fontSize, fontFamily:fontFamily, fontColor:fontColor});
         port.onMessage.addListener((response) => {
+            alert(response.result);
             if(response.result==="success"){
                 console.log("success");
             }
         });
     });
+    window.close();
 }
 
-document.addEventListener('DOMContentLoaded',()=>{
-    console.log("aaaaa");
+
+
+document.addEventListener('DOMContentLoaded',function(){
     var ok_button = document.getElementById('ok_button');
     ok_button.addEventListener('click',connect);
+    
 });
